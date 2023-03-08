a4fee38aa7a05fa24c7440ff3e040aea7e74492a
NethanWin <127036972+NethanWin@users.noreply.github.com>
2023-03-08 18:03:00 +0200

    Start implementing better mult

--- matrix_calc.py
+++ matrix_calc.py
@@ -11,16 +11,18 @@ def add(a,b):
     
     
 def mult(a,b):
-    if type(b) == list and (type(a) == float or type(a) == int):
+    if is_matrix(b)and not is_matrix(a):
         temp = b
         b = a
         a = temp
         
-    if type(a) == list and (type(b) == float or type(b) == int):
+    if is_matrix(a) and not is_matrix(b):
         return [[a[i][j] * b for j in range(len(a[0]))] for i in range(len(a))]
         
     if type(a) == float and type(b) == float:
         return a*b
+        
+        
         
     ai = len(a)
     aj = len(a[0])
@@ -38,6 +40,13 @@ def mult(a,b):
                 c[i][j] += a[i][k] * b[k][j]
     return c
 
+
+def pow(a, num):
+    answear = a
+    for i in range(num-1):
+        answear = mult(answear, a)
+    return answear
+    
     
 def print_matrix(a):
     print('[')
@@ -50,6 +59,66 @@ def print_matrix(a):
     print()
 
 
+def is_matrix(a):
+    return type(a) == list and type(a[0]) == list
+    
+
+def get_new_length(a, b):
+    
+    max_len = max
+    
+    
+def add_zeroes(a, length):
+    # returns 2^n length matrix (adds zeroes)
+    c = [[0 for _ in length] for _ in length]
+    
+    for row in range(len(a)):
+        for col in range(len(a[0])):
+            c[row][col] = a[row][col]
+                
+    return c
+    
+    
+def new_mult(a, b):
+    length = get_new_length(a, b)
+    a = add_zeroes(a, length)
+    b = add_zeroes(b, length)
+    print(b)
+    
+    
+def recursive_mult(m1, m2):
+    # gets 2^n 
+    a = m1[0][0]
+    b = m1[0][1]
+    c = m1[1][0]
+    d = m1[1][1]
+    
+    A = m2[0][0]
+    C = m2[0][1]
+    B = m2[1][0]
+    D = m2[1][1]
+    
+    u = (c-a)(C-D)
+    v = (c+d)(C-A)
+    w = aA+(c+d-a)(A+D-C)
+    
+    
+    
+    
+def calc(txt):
+    #TODO condese string
+    #todo recursive txt
+    if txt[0] == 'a':
+        arg1 = a
+    
+    if txt[1] == 'b':
+        arg2 = b
+        
+    if txt[1] == '+':
+        return add(arg1, arg2)
+    return [[]]
+    
+
 def main():
     a = [[0,1,2,3],
         [85,33554,2,1]]
@@ -57,14 +126,13 @@ def main():
     b = [[1,85,8],
         [5,6,7],
         [3,2,9],
-        [7,84,2]]
+        ]
     
-    print_matrix(mult(a,0))
+   #print_matrix(mult(a,0))
     
-    print_matrix(mult(5.5,b))
-    print_matrix(mult(a,b))
-    txt = input("enter matrix calculation\n")
-    
+    #print_matrix(mult(5.5,b))
+    #print_matrix(mult(a,b))
+    print_matrix(pow(b,82))
 
 if __name__ == '__main__':
     main()
